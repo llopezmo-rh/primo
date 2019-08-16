de110f12763e7d4196651a79394a3223355a6abb
Lucas Lopez Montero <llopezmo@llopezmo.remote.csb>
2019-08-16 15:06:55 +0200

    Some variable, arguments and function names changed

--- primos.c
+++ primos.c
@@ -8,7 +8,7 @@
 #define STRINGIFY(s) _STRINGIFY(s)
 
 // find_divisor returns 0 (false) if no divisor different to 1 has been found
-int find_divisor(mpz_t divisor, const mpz_t i)
+int find_divisor(mpz_t divisor, const mpz_t n)
 	{
 	
 	
@@ -16,28 +16,28 @@ int find_divisor(mpz_t divisor, const mpz_t i)
 
 int main()
 	{
-	char iStr[MAX_LENGTH] = ""STRINGIFY(DEFAULT_FIRST)"";
+	char i_str[MAX_LENGTH] = ""STRINGIFY(DEFAULT_FIRST)"";
 	mpz_t i;
 	mpz_init(i);
-	mpz_init_set_str(i, iStr, 10)
+	mpz_init_set_str(i, i_str, 10)
 	
 	if (mpz_even_p(i))
 		mpz_add_ui(i, i, 1);
 	
-	char divisorStr[MAX_LENGTH];
+	char divisor_str[MAX_LENGTH];
 	mpz_t divisor;
 	for(;;)
 		{
 		if (find_divisor(divisor, i))
 			{
-			mpz_get_str(divisorStr, 10, divisor);
-			mpz_get_str(iStr, 10, i);			
-			printf("%s is divisible by %s\n", iStr, divisorStr);
+			mpz_get_str(divisor_str, 10, divisor);
+			mpz_get_str(i_str, 10, i);			
+			printf("%s is divisible by %s\n", i_str, divisor_str);
 			}
 		else
 			{
-			mpz_get_str(iStr, 10, i);
-			printf("%s IS PRIME\n", iStr);
+			mpz_get_str(i_str, 10, i);
+			printf("%s IS PRIME\n", i_str);
 			break;
 			}
 		mpz_clear(divisor);
