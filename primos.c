85da87deba253d73c761c1fc4e986ac52f16461c
Lucas Lopez Montero <llopezmo@llopezmo.remote.csb>
2019-08-16 12:22:48 +0200

    Function main() finished

--- primos.c
+++ primos.c
@@ -1,24 +1,50 @@
 #include <gmp.h>
 #include <stdio.h>
 
-//#define MAX_LENGTH 100
-#define DEFAULT_FIRST "100000"
+#define MAX_LENGTH 100
+#define DEFAULT_FIRST "1000000"
+
+// find_divisor returns 0 (false) if no divisor has been found
+int find_divisor(mpz_t divisor, const mpz_t i)
+	{
+	
+	
+	}
 
 int main()
 	{
-	mpz_t nFirst;
-	char nStr[] = DEFAULT_FIRST;
+	char iStr[MAX_LENGTH] = DEFAULT_FIRST;
+	register mpz_t i;
+	mpz_init(i);
+	mpz_init_set_str(i, iStr, 10)
 	
-	mpz_init(nFirst);
-	mpz_set_ui(nFirst,0);
+	if (mpz_even_p (i))
+		mpz_add_ui (i, i, 1);
 	
-	mpz_t dummy;
-	mpz_init(dummy);
-	mpz_set_ui(dummy,0);
-	if (mpz_mod_ui (dummy, nFirst, 2)%2 == 0)
-		mpz_sub_ui (nFirst, nFirst, 1);
-		
-	
-	
-	
+	char divisorStr[MAX_LENGTH];
+	mpz_t divisor;
+	for(;;)
+		{
+		if (find_divisor(divisor, i))
+			{
+			mpz_get_str (divisorStr, 10, divisor);
+			mpz_get_str (iStr, 10, i);			
+			printf("%s is divisible by %s\n", iStr, divisorStr);
+			}
+		else
+			{
+			mpz_get_str (iStr, 10, i);
+			printf("%s IS PRIME\n", iStr);
+			break;
+			}
+		mpz_add_ui (i, i, 2);
+		if (mpz_sizeinbase (i, 10)>MAX_LENGTH)
+			{
+			fprintf(stderr, "Maximum number length reached: %d ciphers\n", MAX_LENGTH);
+			break;
+			}
+		}
+	mpz_clear (i);
+	mpz_clear (divisor);
+	return 0;	
 	}
