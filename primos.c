a3de850e3cc37f4a994d47968d3b4cd879b29650
Lucas <lucas@lenovo-g580>
2019-08-18 16:08:59 +0200

    getopt added

--- primos.c
+++ primos.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
 #include <gmp.h>
 
 // If the loop reaches a number with more ciphers than MAX_LENGTH, it will stop
@@ -54,23 +57,51 @@ int main(int argc, char *argv[])
 	// Removing buffer to send output to stdout immediately
 	setbuf(stdout, NULL);
 	
+	// Getting options
+	int opt;
+	int prime_break = 0;
+	unsigned long int n_output = 0;
+	while ((opt = getopt (argc, argv, "bn:")) != -1)
+		switch (opt)
+			{
+			case 'b':
+				prime_break = 1;
+				break;
+			case 'n':
+				n_output = strtoul(optarg, NULL, 10);
+				break;
+			case '?':
+				// First option does not seem to be necessary
+				if (optopt == 'n')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				printf("Usage: %s [-b] [-n <n_output>] [starting_value]\n", argv[0]);
+				return 1;
+			default:
+				printf("Usage: %s [-b] [-n <n_output>] [starting_value]\n", argv[0]);
+				return 1;
+			}	
+	
 	// Setting first value of "i", which will be the loop index
 	mpz_t i;
-	if (argc == 1)
-		mpz_init_set_str(i, DEFAULT_FIRST, 10);
+	if (optind<argc)
+		mpz_init_set_str(i, argv[optind], 10);
 	else
-		mpz_init_set_str(i, argv[1], 10);
+		mpz_init_set_str(i, DEFAULT_FIRST, 10);		
 		
 	// Validating first value of "i"
 	if (mpz_cmp_ui(i, 2) < 0)
 		{
 		fprintf(stderr, "Fatal error: starting number must be an integer greater than or equal to 2\n");
-		return 1;
+		return 2;
 		}		
 	if (mpz_sizeinbase(i, 10)>MAX_LENGTH)
 		{
 		fprintf(stderr, "Fatal error: number cannot have more than "STRINGIFY(MAX_LENGTH)" ciphers\n");
-		return 2;
+		return 3;
 		}
 	
 	// Making sure that first value of "i" is odd
@@ -82,6 +113,7 @@ int main(int argc, char *argv[])
 	char i_str[MAX_LENGTH];
 	char divisor_str[MAX_LENGTH];
 	mpz_t divisor;
+	int count = 1;
 	do
 		{
 		if (find_divisor(divisor, i))
@@ -94,10 +126,13 @@ int main(int argc, char *argv[])
 			{
 			mpz_get_str(i_str, 10, i);
 			printf("%s IS PRIME\n", i_str);
-			//break;
+			if (prime_break)
+				break;
 			}
 		mpz_clear(divisor);
 		mpz_add_ui(i, i, 2);
+		if (n_output>0 && ++count>n_output)
+			break;
 		} while (mpz_sizeinbase(i, 10)<=MAX_LENGTH);
 		
 	//mpz_clear(i);
